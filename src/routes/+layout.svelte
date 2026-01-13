d8565e1d15d0e1a285bade2fc216c4993ca3f1b7
상만 <a01071397193@gmail.com>
2026-01-13 16:42:15 +0900

    feat: log tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,12 @@
 	import './layout.css';
     import "../app.css";
 	import favicon from '$lib/assets/favicon.svg';
+    import {inject} from "@vercel/analytics";
+    import {dev} from "$app/environment";
 
 	let { children } = $props();
+
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
